fc19199bea5b67e31ff9bb2527affd53b8bad256
phyu007 <sow17809@gmail.com>
2020-05-04 20:33:11 +0800

    some updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/out-tsc'));
+app.use(express.static(__dirname + '/dist/out-tsc/myApp'));
 
 // Send all requests to index.html
 app.get('/*', function (req, res) {
--- src/app/overall/overall.component.ts
+++ src/app/overall/overall.component.ts
@@ -15,7 +15,6 @@ export class OverallComponent implements OnInit {
 
   @ViewChild('sourceCanvas') sourceCanvasRef: ElementRef<HTMLCanvasElement>;
   @ViewChild('targetCanvas') targetCanvasRef: ElementRef<HTMLCanvasElement>;
-  @ViewChild('targetCanvass') targetCanvasReff: ElementRef<HTMLCanvasElement>;
 
    format = 'MMM/yyyy';
     locale = 'en-US';
@@ -920,92 +919,6 @@ export class OverallComponent implements OnInit {
 
         
      
-    const sourceCanvas = this.sourceCanvasRef.nativeElement;
-    const sourceCtx = sourceCanvas.getContext('2d');
-    const targetCanvas = this.targetCanvasRef.nativeElement;
-    const targetCanvass =this.targetCanvasReff.nativeElement;
-    const targetCtx = targetCanvas.getContext('2d');
- 
-    const targetCtxx = targetCanvass.getContext('2d');
-      
-    targetCtx.clearRect(0, 0, targetCanvas.width, targetCanvas.height);
-  var myChart = new Chart(sourceCtx, {
-      type: 'line',      
-      
-      data: {
-        datasets: [{
-          label: 'Höhenlinie',
-          backgroundColor: "rgba(255, 99, 132,0.4)",
-          borderColor: "rgb(255, 99, 132)",
-          fill: true,
-          data: this.data,
-        }]
-      },
-      options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'Höhenlinie'
-        },
-        scales: {
-          xAxes: [{
-            type: 'linear',
-            position: 'bottom',
-
-            ticks:{
-              // Include a dollar sign in the ticks
-        callback: (tick) => {
-          if (tick >= 1000) {
-            return (tick / 1000).toString() + ' km';
-          }
-          return tick.toString() + ' m';
-        }
-
-          },
-           
-            scaleLabel: {
-              labelString: 'Länge',
-              display: true,
-            }
-          }],
-          yAxes: [{
-            type: 'linear',
-            ticks: {
-              callback: (tick) => tick.toString() + ' m'
-            },
-            scaleLabel: {
-              labelString: 'Höhe',
-              display: true
-            }
-          }],
-        },          
-        animation: {
-          onComplete: function() {
-            if (!this.rectangleSet) {
-              const scale = window.devicePixelRatio;
-              const copyWidth = myChart['y-axis-0'].width - 10;
-              const copyHeight = myChart['y-axis-0'].height + myChart['y-axis-0'].top + 10;
-              
-              targetCtx.scale(scale, scale);
-              targetCtx.canvas.width = copyWidth * scale;
-              targetCtx.canvas.height = copyHeight * scale;
-              targetCtx.canvas.style.width = copyWidth + 'px';
-              targetCtx.canvas.style.height = copyHeight + 'px';
-              targetCtx.drawImage(sourceCanvas, 0, 0, copyWidth * scale, copyHeight * scale, 0, 0,copyWidth * scale, copyHeight * scale);
-              sourceCtx.clearRect(0, 0, copyWidth, copyHeight);
-              this.rectangleSet = true;
-            }
-          },
-          onProgress: function() {
-            if (this.rectangleSet) {
-              var copyWidth = myChart['y-axis-0'].width;
-              var copyHeight = myChart['y-axis-0'].height + myChart['y-axis-0'].top + 10;
-              sourceCtx.clearRect(0, 0, copyWidth, copyHeight);
-            }
-          },
-        }
-      }
-    });
 
       var ctx = document.getElementById("myChart")  as HTMLCanvasElement;;
 
@@ -1035,9 +948,9 @@ export class OverallComponent implements OnInit {
               var sourceCanvas = this.chart.ctx.canvas;
               var copyWidth = this.scale.xScalePaddingLeft - 5;
               var copyHeight = this.scale.endPoint + 5;
-          //    targetCtxx= document.getElementById("myChartAxis") as HTMLCanvasElement;
-              targetCtxx.canvas.width = copyWidth;
-              targetCtxx.drawImage(sourceCanvas, 0, 0, copyWidth, copyHeight, 0, 0, copyWidth, copyHeight);
+              var targetCtx= document.getElementById("myChartAxis") as HTMLCanvasElement;
+               targetCtx.canvas.width = copyWidth;
+             targetCtx.drawImage(sourceCanvas, 0, 0, copyWidth, copyHeight, 0, 0, copyWidth, copyHeight);
       
             }
           }
