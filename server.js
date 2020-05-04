2adcd5f38e00c75a0f151a061e6b77646d2393a1
phyu007 <sow17809@gmail.com>
2020-05-04 20:34:51 +0800

    some updates

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/out-tsc/myApp'));
+app.use(express.static(__dirname + '/dist/FRONTEND'));
 
 // Send all requests to index.html
-app.get('/*', function (req, res) {
-    res.sendFile(path.join());
-});
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+
+    '/dist/FRONTEND/index.html'));});
+    
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
