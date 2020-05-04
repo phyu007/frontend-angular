d0c0ec21923a0fd6823c787eb7fb141dfd11e852
phyu007 <sow17809@gmail.com>
2020-05-04 17:36:40 +0800

    some updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/frontend'));
+ngApp.use(express.static('./dist/out-tsc/src/app'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, './dist/frontend/index.html'));
+    response.sendFile(path.join(__dirname, './dist/out-tsc/src/app/excess-inventory/excess-inventory.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
