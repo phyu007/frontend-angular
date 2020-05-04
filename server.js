53f285f63a2159ea569c97f7ffc83778a992d63c
phyu007 <sow17809@gmail.com>
2020-05-04 16:42:39 +0800

    some updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./src/index.html'));
+ngApp.use(express.static('./dist/app/excess-inentory'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, './src/index.html'));
+    response.sendFile(path.join(__dirname, './dist/app/excess-inentory'));
 });
 ngApp.listen(process.env.PORT || 8080);
